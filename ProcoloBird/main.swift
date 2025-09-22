7a938c6d1cbf91f4d59d3a44169cd50c6c1837bf
parisollie <pjff18@gmail.com>
2025-09-22 15:42:48 -0600

    Initial Commit

---
+++ ProcoloBird/main.swift
@@ -0,0 +1,87 @@
+//
+//  main.swift
+//  ProcoloBird
+//
+//  Created by Paul F on 22/09/25.
+//
+
+import Foundation
+
+// Protocol defining the ability to fly
+protocol CanFly {
+    func fly()
+}
+
+/**I'm going to create a new class and my class is going to be a Bird class
+and I'm going to define a bunch of things that birds can do.*/
+class Bird {
+    // Property to track if the bird is female
+    var isFemale = true
+    
+    func layEgg() {
+        /***
+         Now, in addition, if a bird is female, then they can also lay eggs, right?
+         So let's create this layEgg functionality where we check to make sure. If our bird is female, then in
+         that case, we can go ahead and print "The bird makes a new bird in a shell."
+         
+         */
+        if isFemale {
+            // Only female birds can lay eggs
+            print("The bird makes a new bird in a shell.")
+        }
+    }
+}
+
+// Eagle class inherits from Bird and conforms to CanFly protocol
+class Eagle: Bird, CanFly {
+    // Implementation of the fly method from CanFly protocol
+    func fly() {
+        print("The bird flaps its wings and lifts off into the sky.")
+    }
+    
+    // Eagle-specific method for soaring
+    func soar() {
+        print("The eagle flaps its wings and lifts off into the sky.")
+    }
+}
+
+// Penguin class inherits from Bird but cannot fly
+class Penguin: Bird {
+    // Penguin-specific method for swimming
+    func swim() {
+        print("The penguin paddles through the water")
+    }
+}
+
+// Struct that demonstrates flying capability
+struct FlyingMuseum {
+    // Method that takes any object conforming to CanFly protocol
+    func flyingDemo(flyingObject: CanFly) {
+        // Calls the fly method on the flying object
+        flyingObject.fly()
+    }
+}
+
+// Airplane struct conforms to CanFly protocol (not a bird)
+struct Airplane: CanFly {
+    // Implementation of fly method for airplane
+    func fly() {
+        print("The airplane uses its engine to lift off into the air")
+    }
+}
+
+// Create instances of the classes and struct
+let myEagle = Eagle()      // Eagle instance
+let myPenguin = Penguin()  // Penguin instance
+let myPlane = Airplane()   // Airplane instance
+
+// Demonstrate flying and swimming capabilities
+myEagle.fly()      // Eagle can fly
+myPenguin.swim()   // Penguin can swim
+
+// Create FlyingMuseum instance
+let museum = FlyingMuseum()
+
+// Demonstrate flying with different objects that conform to CanFly
+museum.flyingDemo(flyingObject: myEagle)   // Eagle demonstrates flying
+museum.flyingDemo(flyingObject: myPlane)   // Airplane demonstrates flying
